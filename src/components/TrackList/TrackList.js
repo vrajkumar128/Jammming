762a35a8e490c9bdfe91338a316117e23e2b5e6c
vrrajkum <vivek.rajkumar@protonmail.com>
2018-04-19 10:57:07 -0400

    Add and style Track component.

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -5,7 +5,7 @@ export class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {/*<!-- You will add a map method that renders a set of Track components  -->*/}
+        {/* You will add a map method that renders a set of Track components */}
       </div>
     )
   }
